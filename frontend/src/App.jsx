4e5422e18beb061c3bf94eb8d9c924760b5669b6
Jerald Wilks <jerald@wilks.pro>
2023-04-17 14:39:41 -0600

    Added bootstrap css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
 import Map from "./Map";
+// import TourMap from "./TourMap";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
