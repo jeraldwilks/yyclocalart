2095f6514fbf3f03044dbe5fc2ea2cbbb66021ff
Jerald Wilks <jerald@wilks.pro>
2023-04-18 13:13:29 -0600

    Testing TourMap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 // import "./App.css";
-import Map from "./Map";
-// import TourMap from "./TourMap";
+// import Map from "./Map";
+import TourMap from "./TourMap";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div>
-      <Map />
+      <TourMap />
     </div>
   );
 }
---
+++ frontend/src/TourMap.jsx
@@ -0,0 +1,140 @@
+import React, { useRef, useEffect, useState } from "react";
+import "./Map.css";
+import mapboxgl from "mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import "mapbox-gl/dist/mapbox-gl.css";
+// import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+
+mapboxgl.accessToken = import.meta.env.VITE_MAP_TOKEN;
+
+const TourMap = () => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  let prevArray = useRef(null);
+  const [lng, setLng] = useState(-114.0571411);
+  const [lat, setLat] = useState(51.0453775);
+  const [zoom, setZoom] = useState(13);
+  const [tourLocations, setTourLocations] = useState([]);
+  console.log("Load TourMap()");
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/outdoors-v12",
+      center: [lng, lat],
+      zoom: zoom,
+    })
+      .addControl(
+        new mapboxgl.GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true,
+          },
+          // When active the map will receive updates to the device's location as it changes.
+          trackUserLocation: true,
+          // Draw an arrow next to the location dot to indicate which direction the device is heading.
+          showUserHeading: true,
+          showUserLocation: true,
+        })
+      )
+      .addControl(new mapboxgl.NavigationControl());
+    console.log("Load useEffect()");
+    // .addControl(
+    //   new MapboxGeocoder({
+    //     accessToken: mapboxgl.accessToken,
+    //     mapboxgl: mapboxgl,
+    //   })
+    // );
+    map.current.on("load", () => {
+      map.current.addSource("art", {
+        type: "geojson",
+        data: "/api/geojson",
+      });
+      map.current.loadImage("icon.png", (error, image) => {
+        if (error) throw error;
+
+        // Add the image to the map style.
+        map.current.addImage("icon", image);
+      });
+      map.current.addLayer({
+        id: "art",
+        type: "symbol",
+        source: "art",
+        layout: {
+          "icon-image": "icon",
+          "icon-size": 0.075,
+          "icon-allow-overlap": true,
+          // get the title name from the source's "title" property
+          "text-field": ["get", "title"],
+          "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+          "text-offset": [0, 1.25],
+          "text-anchor": "top",
+        },
+      });
+    });
+    map.current.on("click", "art", (e) => {
+      // Copy coordinates array.
+      // console.log(e.features[0]);
+      const coordinates = e.features[0].geometry.coordinates.slice();
+      const description = e.features[0].properties.short_desc;
+      const title = e.features[0].properties.title;
+      const address = e.features[0].properties.address;
+      // Ensure that if the map is zoomed out such that multiple
+      // copies of the feature are visible, the popup appears
+      // // over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+      // console.log(e);
+      // console.log(coordinates);
+      setTourLocations((prevArray) => [...prevArray, e.features[0]]);
+      console.log("tourLocations:");
+      console.log(tourLocations);
+
+      // new mapboxgl.Popup()
+      //   .setLngLat(coordinates)
+      //   .setHTML(
+      //     "<h2>" +
+      //       title +
+      //       "</h2><h3>" +
+      //       address +
+      //       "</h3><p>" +
+      //       description +
+      //       "</p>"
+      //   )
+      //   .addTo(map.current);
+    });
+
+    // Change the cursor to a pointer when the mouse is over the places layer.
+    map.current.on("mouseenter", "art", () => {
+      map.current.getCanvas().style.cursor = "pointer";
+    });
+
+    // Change it back to a pointer when it leaves.
+    map.current.on("mouseleave", "art", () => {
+      map.current.getCanvas().style.cursor = "";
+    });
+  });
+
+  return (
+    <div>
+      <div ref={mapContainer} className="map-container" />
+      <h1>Tour Stops:</h1>
+      {tourLocations.length === 0 && <p>No locations added.</p>}
+      <ul className="list-group">
+        {tourLocations.map((location) => (
+          <li
+            className="list-group-item"
+            key={location.properties.art_id}
+            onClick={() => {
+              console.log(location);
+            }}
+          >
+            {location.properties.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TourMap;
