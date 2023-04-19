703bd40e75a06eba2a492c821552153d2e6dfecc
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-04-19 11:58:48 -0600

    added header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Map from "./Map";
+import Header from "./Header.jsx";
 
 function App() {
   return (
     <div>
+      <Header />
       <Map />
     </div>
   );
