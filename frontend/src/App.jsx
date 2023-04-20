fbbd7125fddb090e85819b23c1d6a1dc3a84578f
Jerald Wilks <jerald@wilks.pro>
2023-04-20 09:38:28 -0600

    Setup Router & Moved to components folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
 // import "./App.css";
-import Map from "./Map";
+import Map from "./components/Map";
+import AboutUs from "./components/AboutUs";
 // import TourMap from "./TourMap";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div>
-      <Map />
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About Us</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Map />} />
+        <Route path="/about" element={<AboutUs />} />
+      </Routes>
     </div>
   );
 }
