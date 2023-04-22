fe23ddc52ef50779dc68b06be5471a0c1d293c4e
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-04-22 16:46:20 -0600

    Added teamMembers contact info for about page

---
+++ frontend/src/teamMembers.js
@@ -0,0 +1,18 @@
+const teamMembers = [
+  {
+    name: "Jennifer Gajdos-Bomben",
+    email: "jgajdosbomben@gmail.com",
+  },
+  {
+    name: "Jerald Wilks",
+    email: "jerald@wilks.pro",
+  },
+  {
+    name: "Jonah Turning Robe",
+    email: "jjturningrobe@gmail.com",
+  },
+  {
+    name: "Kanchan Samrit",
+    email: "samrit.kanchan76@gmail.com",
+  },
+];
