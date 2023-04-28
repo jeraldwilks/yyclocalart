c7fc88bc81ea08b35c5f3f6f33cca3bfddff09cf
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-04-27 21:51:39 -0600

    Added grid item and style

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -17,8 +17,23 @@ function AboutApp() {
 console.log(teamMembers);
 
 return (
-  <Grid container spacing={4} rowSpacing={2} columnSpacing={{ xs: 1, sm: 4, md: 6}}>
+  <Grid container spacing={6} justify="center">
+    <Grid item xs={10} s={6} md={6}
+    direction="row"
+    maxWidth="425"
+    justify="space-evenly"
+    alignContent="center"
+    alignItems="center"
+    >
+  {/* direction="row"
+  rowSpacing={2}
+  columnSpacing={{ xs: 12, sm: 6, md:6, lg: 6, xl: 6}}
+  item
+  // alignItems="center"
+  // alignContent="center"
+  // justify="center" */}
 {teamMembers.map(createCard)}
+  </Grid>
   </Grid>
 );
 }
