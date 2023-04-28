f5090df1769605275b678e9ee4670f5b52af84a4
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-04-27 21:48:52 -0600

    Added home page

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../index.css"
+
+const Home = () => {
+  return (
+    <div className="home">
+       <h1>Welcome to YYC Local Art</h1> 
+       <h2> Bringing awareness to local Calgary public art locations</h2>
+    </div>
+  )
+}
+
+export default Home;
