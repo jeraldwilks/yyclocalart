82d4f5acae6b2dc1261f3238099cb78e61eadf5b
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-04-29 11:50:20 -0600

    Added images property

--- frontend/src/teamMembers.js
+++ frontend/src/teamMembers.js
@@ -3,21 +3,25 @@ const teamMembers = [
     id: 1,
     name: "Jennifer Gajdos-Bomben",
     email: "jgajdosbomben@gmail.com",
+    image: "./images/image-jennifer.jpg"
   },
   {
     id: 2,
     name: "Jerald Wilks",
     email: "jerald@wilks.pro",
+    image: "./images/image-jerald.jpeg"
   },
   {
     id: 3,
     name: "Jonah Turning Robe",
     email: "jjturningrobe@gmail.com",
+    image: "image"
   },
   {
     id: 4,
     name: "Kanchan Samrit",
     email: "samrit.kanchan76@gmail.com",
+    image: "./images/image-kanchan.jpeg"
   },
 ];
 
