878da44fea538107948653c2a43970d0f4360a1d
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-05-02 20:28:06 -0600

    Moved images folder inside of public folder

--- frontend/src/teamMembers.js
+++ frontend/src/teamMembers.js
@@ -3,26 +3,27 @@ const teamMembers = [
     id: 1,
     name: "Jennifer Gajdos-Bomben",
     email: "jgajdosbomben@gmail.com",
-    image: "./images/image-jennifer.jpg"
+    image: "/public/images/image-jennifer.jpg"
   },
+  // image: "./images/image-jennifer.jpg"
   {
     id: 2,
     name: "Jerald Wilks",
     email: "jerald@wilks.pro",
-    image: "./images/image-jerald.jpeg"
+    image: "/public/images/image-jerald.jpeg"
   },
   {
     id: 3,
     name: "Jonah Turning Robe",
     email: "jjturningrobe@gmail.com",
-    image: "./images/image-jonah.jpg"
+    image: "/public/images/image-jonah.jpg"
   },
   {
     id: 4,
     name: "Kanchan Samrit",
     email: "samrit.kanchan76@gmail.com",
-    image: "./images/image-kanchan.jpeg"
-  },
+    image: "/public/images/image-kanchan.jpeg"
+   },
 ];
 
 export default teamMembers;
