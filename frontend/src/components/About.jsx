caf0cda8decf5bb3ef24eeb3e842a8e9ecf6e7bd
Jerald Wilks <jerald@wilks.pro>
2023-05-03 16:00:12 -0600

    Remove console.log

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,38 +1,38 @@
 import teamMembers from "../teamMembers";
-import AboutCard from "./AboutCard"
+import AboutCard from "./AboutCard";
 import { Grid } from "@mui/material";
-
 
 function createCard(teamMember) {
   return (
     <AboutCard
-    key={teamMember.id}
-    name={teamMember.name}
-    image={teamMember.image}
-    email= {<a href={`mailto: ${teamMember.email}`}>{teamMember.email}</a>}
+      key={teamMember.id}
+      name={teamMember.name}
+      image={teamMember.image}
+      email={<a href={`mailto: ${teamMember.email}`}>{teamMember.email}</a>}
     />
-    );
-  }
-  
-  function AboutApp() {
-    console.log(teamMembers);
-    
-    return (
-      <Grid container spacing={1} justifyContent="center" marginLeft={1}>
-  
-{teamMembers.map((person) => (
-  <Grid item xs={12} sm={6} md={3}
-  direction="row"
-  maxWidth="400"
-  justifyContent="space-evenly"
-  alignContent="center"
-  // alignItems="center"
-  >
-    {createCard(person)}
-  </Grid>
-))}
-  </Grid>
-);
+  );
+}
+
+function AboutApp() {
+  return (
+    <Grid container spacing={1} justifyContent="center" marginLeft={1}>
+      {teamMembers.map((person) => (
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          md={3}
+          direction="row"
+          maxWidth="400"
+          justifyContent="space-evenly"
+          alignContent="center"
+          // alignItems="center"
+        >
+          {createCard(person)}
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
 
 export default AboutApp;
