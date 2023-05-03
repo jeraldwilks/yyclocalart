a0b44b063a703b2cf9cd2216d432843a383bc124
Jerald Wilks <jerald@wilks.pro>
2023-05-03 11:50:27 -0600

    Change server to use public again

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4001;
 const app = express();
 app.use(express.json());
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-app.use(express.static("../frontend/dist"));
+app.use(express.static("public"));
 
 app.use("/api/geojson", geojsonRouter);
 app.use("/api/route", routeRouter);
